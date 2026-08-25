cca0aa682ac0aa717cdb25bf93fe67878d2af702
User <nikn63641@gmail.com>
2026-08-25 11:39:08 +0530

    Connect backend to MongoDB Atlas cluster and seed properties

--- seedProperties.js
+++ seedProperties.js
@@ -1,3 +1,5 @@
+const dns = require('dns');
+try { dns.setServers(['8.8.8.8', '1.1.1.1']); } catch(e) {}
 const mongoose = require('mongoose');
 const Property = require('./models/Property');
 const User = require('./models/User');
